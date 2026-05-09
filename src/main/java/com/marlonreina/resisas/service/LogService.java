72880263efe444a525d075f641cc634213543be2
marliton12 <velezmarlon72@gmail.com>
2026-05-08 19:40:22 -0500

    feat:DJB-16 acomodacion de checkStyle en logService

--- src/main/java/com/marlonreina/resisas/service/LogService.java
+++ src/main/java/com/marlonreina/resisas/service/LogService.java
@@ -31,7 +31,9 @@ public class LogService {
         getConfig(guild.getId()).ifPresent(config -> {
             if (config.getChannelId() != null && config.getEnabled()) {
                 var channel = guild.getTextChannelById(config.getChannelId());
-                if (channel != null) channel.sendMessage(message).queue();
+                if (channel != null){
+                    channel.sendMessage(message).queue();
+                }
             }
         });
     }
